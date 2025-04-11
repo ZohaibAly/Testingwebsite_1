7e9ad7d684ef365cbc8a9ced05d95e76a3226df3
ZohaibDeveltro <zohaibdeveltro@gmail.com>
2025-04-11 19:21:48 +0500

    Before After ShowCase component finalize

--- src/Pages/BeforeAfter/BeforeAfter.jsx
+++ src/Pages/BeforeAfter/BeforeAfter.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './BeforeAfter.css';
 import Testmonial from '../../Component/TestimonialSection/TestimonialSection';
 import FreeStoreComp from '../../Component/FreeStore/FreeStore';
+import BeforeAfterShowCase from '../../Component/BeforeAfterShowcase/BeforeAfterShowcase';
 
 function BeforeAfter() {
     return (
         <div className="BeforeAfter">
+            <BeforeAfterShowCase/>
             <Testmonial/>
             <FreeStoreComp/>
         </div>
