5330ba7951215367055219eb81b58afd7bbd1786
ZohaibDeveltro <zohaibdeveltro@gmail.com>
2025-04-12 00:27:16 +0500

    import BreadCrumbComnponent on careers and FAQs page

--- src/Pages/Careers/Careers.jsx
+++ src/Pages/Careers/Careers.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './Careers.css';
 import Testmonial from '../../Component/TestimonialSection/TestimonialSection';
 import FreeStoreComp from '../../Component/FreeStore/FreeStore';
-
+import BreadcrumbBanner from '../../Component/BreadcrumbBanner/BreadcrumbBanner';
 function Careers() {
     return (
         <div className="Careers">
+            <BreadcrumbBanner/>
             <Testmonial/>
             <FreeStoreComp/>
         </div>
--- src/Pages/FAQ/FAQ.jsx
+++ src/Pages/FAQ/FAQ.jsx
@@ -3,10 +3,11 @@ import './FAQ.css';
 import Testmonial from '../../Component/TestimonialSection/TestimonialSection';
 import FreeStoreComp from '../../Component/FreeStore/FreeStore';
 import FAQSection from '../../Component/FAQ/FAQ';
-
+import BreadcrumbBanner from '../../Component/BreadcrumbBanner/BreadcrumbBanner';
 function FAQ() {
     return (
         <div className="FAQ">
+            <BreadcrumbBanner/>
             <FAQSection />
             <Testmonial/>
             <FreeStoreComp/>
