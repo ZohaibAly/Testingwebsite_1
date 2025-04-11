259e6ab6030062f233ec71e1cb2994251a8c78c7
Muhammad-Bilal-Arif-123 <commercekind7861@gmail.com>
2025-04-12 00:18:22 +0500

    minor changes

--- src/Pages/CaseStudies/CaseStudies.jsx
+++ src/Pages/CaseStudies/CaseStudies.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './CaseStudies.css';
 import Testmonial from '../../Component/TestimonialSection/TestimonialSection';
 import FreeStoreComp from '../../Component/FreeStore/FreeStore';
+import CSSeection from '../../Component/CaseStudies/CaseStudies'
 
 function CaseStudies() {
     return (
         <div className="CaseStudies">
+            <CSSeection />
             <Testmonial/>
             <FreeStoreComp/>
         </div>
