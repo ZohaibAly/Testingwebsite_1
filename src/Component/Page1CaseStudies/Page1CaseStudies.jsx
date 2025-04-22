3adf31d17a719a33be6113c0d969b85952d1ca48
ZohaibDeveltro <zohaibdeveltro@gmail.com>
2025-04-22 17:38:27 +0500

    working on page1 two section complete

--- src/Component/Page1CaseStudies/Page1CaseStudies.jsx
+++ src/Component/Page1CaseStudies/Page1CaseStudies.jsx
@@ -13,14 +13,16 @@ import MaskGroup from '../../assets/Mask-group.png';
 
 const CaseStudiesPage1 = () => {
   return (
-    <div className="case-study-container">
-      {/* Background ellipse */}
-      <div className="ellipse-container">
-        <img src={Ellipse} alt="Background glow" className="background-ellipse" />
-      </div>
 
-      {/* Section 1: Brand Overview */}
+    <div className='CaseStudiesPage1-comp'>
+
+    <div className="Page1-top-brand">
+  
+
       <div className="brand-overview-section">
+        <div className='background-ellipse'>
+            <img src={Ellipse} alt="" />
+        </div>
         <h1 className="main-title">Strategic Growth for Bariatric Multivitamins Brand Case Study</h1>
         
         <div className="overview-content">
@@ -43,7 +45,45 @@ const CaseStudiesPage1 = () => {
 
      
     </div>
+{/* challenge compoent */}
+
+<div className="challenges-section">
+      <div className="challenges-1">
+        <div className='challenge-content'>
+          <h3>Challenges Before Our Takeover</h3>
+          <p><span> <strong>Sales Stagnation</strong> – Sales were stuck at 3,000 units/month with no structured growth plan.</span></p>
+          <p><span> <strong>S&S Buyer Drop-Off</strong> – Inactive subscriptions due to previous inventory gaps, leading to declining recurring revenue.</span></p>
+          <p><span> <strong>Low Conversions</strong> – Unoptimized content, visuals, and keyword strategy hindered conversion rates.</span></p>
+          <p><span> <strong>PPC Wastage</strong> – Broad targeting and inefficient spending led to a high ACoS without strong returns.</span></p>
+          <p><span> <strong>Weak Brand Presence</strong> – Competitors dominated key placements, reducing visibility.</span></p>
+        </div>
+        <div className='image-section'>
+          <div className='image-section-left'>
+            <div className='image-section-left-1'>
+            <img src={page1image1} alt="Logo" />
+            <h2>S&S Buyer Drop-Off</h2>
+            </div>
+            <div className='image-section-left-2'>
+            <img src={page1image3} alt="Logo" />
+              <h2>PPC Wastage</h2>
+            </div>
+          </div>
+          <div className='image-section-right'>            
+          <div className='image-section-right-1'>
+          <img src={page1image2} alt="Logo" />
+          <h2>Sales Stagnation and Low Conversions</h2>
+          </div>
+            <div className='image-section-right-2'>
+            <img src={page1image4} alt="Logo" />
+            <h2>Weak Brand Presence</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     
+    </div>
 
 
 
