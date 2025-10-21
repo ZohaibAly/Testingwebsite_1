15faec0ab6ad1a06af2f30e1c0c42c859d89d279
[zohaib <[mzohaibali223@gmail.com]>
2025-10-21 17:43:00 +0500

    fixed

--- src/Pages/FAQ/FAQ.jsx
+++ src/Pages/FAQ/FAQ.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './FAQ.css';
-import Testmonial from '../../Component/TestimonialSection/TestimonialSection';
 import FreeStoreComp from '../../Component/FreeStore/FreeStore';
 import FAQSection from '../../Component/FAQ/FAQ';
 import BreadcrumbBanner from '../../Component/BreadcrumbBanner/BreadcrumbBanner';
@@ -9,7 +8,7 @@ function FAQ() {
         <div className="FAQ">
             <BreadcrumbBanner/>
             <FAQSection />
-            <Testmonial/>
+      
             <FreeStoreComp/>
         </div>
     );
