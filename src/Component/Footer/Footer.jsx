455cea35e9242aa5859c9cc05ca1dcbcad0cabe9
[zohaib <[mzohaibali223@gmail.com]>
2025-11-06 01:29:12 +0500

    add whastapp

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,78 +1,121 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.css';
-import frame1 from '../../assets/Logo-HD-JPT.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import frame1 from "../../assets/Logo-HD-JPT.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWhatsapp,
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-    return (
-        <div>
-        <div className='Footer-Parent'>
-            <div className='Footer-main'>
-            <div className='Footerpt-1'>
-                <div>
-                    <img className='image-1' src={frame1} alt="" />
-                </div>
-                <p>
-             JPT AMZ SOLUTIONS represents your reliable collaborator for marketplace achievement. We assist enterprises to expand their channels and portfolios with personalized approaches and demonstrated competence.
-                </p>
-                <div className='Footerpt-1-images'>
-                  <div className='Footer-1-icons'>  
-                  <Link to="#"><FontAwesomeIcon icon={faFacebook} /></Link>
-                  </div>
-               <div className='Footer-1-icons'>
-               <Link to="#"><FontAwesomeIcon icon={faXTwitter}  /></Link>
-               </div>
-              <div className='Footer-1-icons'>
-              <Link to="#"><FontAwesomeIcon icon={faInstagram} /></Link>
+  return (
+    <div>
+      <div className="Footer-Parent">
+        <div className="Footer-main">
+          <div className="Footerpt-1">
+            <div>
+              <img className="image-1" src={frame1} alt="" />
+            </div>
+            <p>
+              JPT AMZ SOLUTIONS represents your reliable collaborator for
+              marketplace achievement. We assist enterprises to expand their
+              channels and portfolios with personalized approaches and
+              demonstrated competence.
+            </p>
+            <div className="Footerpt-1-images">
+              <div className="Footer-1-icons">
+                <Link
+                  to="https://wa.me/17324514720"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faWhatsapp} />
+                </Link>
               </div>
-              <div className='Footer-1-icons'>
-              <Link to="https://www.linkedin.com/in/mindy-israel-a8a015390/"><FontAwesomeIcon icon={faLinkedin} /></Link>
+              <div className="Footer-1-icons">
+                <Link to="#">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </Link>
               </div>
-               
-                </div>
+              <div className="Footer-1-icons">
+                <Link to="#">
+                  <FontAwesomeIcon icon={faXTwitter} />
+                </Link>
+              </div>
+              <div className="Footer-1-icons">
+                <Link to="#">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </Link>
+              </div>
+              <div className="Footer-1-icons">
+                <Link to="https://www.linkedin.com/in/mindy-israel-a8a015390/">
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </Link>
+              </div>
             </div>
-            <div className='Footerpt-2'>
-                <div className='Company'>
-                    <h4>Company</h4>
-                    <Link to="/"><p>Home</p></Link>
-                    <Link to="/case-studies"><p>Case Studies</p></Link>
-                    <Link to="/before-and-after"><p>Before And After</p></Link>
-                    <Link to="/services"><p>Services</p></Link>
-                    <Link to="/contact-us"><p>Contact</p></Link>
-                </div>
-                <div className='Quicklink'>
-                    <h4>Quick Links</h4>
-                    <Link to="/blog"><p>Blog</p></Link>
-                    <Link to="/frequently-asked-questions"><p>FAQs</p></Link>
-           
-                </div>
-                <div className='Newsletter'>
-                    <h4>Others</h4>
-                    <a href=""><p>Terms & Conditions</p></a>
-                    <a href=""><p>Privacy Policy</p></a>
-                </div>
+          </div>
+          <div className="Footerpt-2">
+            <div className="Company">
+              <h4>Company</h4>
+              <Link to="/">
+                <p>Home</p>
+              </Link>
+              <Link to="/case-studies">
+                <p>Case Studies</p>
+              </Link>
+              <Link to="/before-and-after">
+                <p>Before And After</p>
+              </Link>
+              <Link to="/services">
+                <p>Services</p>
+              </Link>
+              <Link to="/contact-us">
+                <p>Contact</p>
+              </Link>
             </div>
-            
-            <div className='Footerpt-3'>
-                <h4>Newsletter</h4>
-                <div className="Footer-input-wrapper">
-                    <input type="text" placeholder="Enter Your Email" className="input-field" />
-                    <button type="button">Subscribe</button>
-                </div>
-                <h4>Phone: </h4>
-                <p>(732) 451-4720</p>
-                <h4>Address: </h4>
-          <p>4392 Riverside Ave Tampa, FL 33614</p>
+            <div className="Quicklink">
+              <h4>Quick Links</h4>
+              <Link to="/blog">
+                <p>Blog</p>
+              </Link>
+              <Link to="/frequently-asked-questions">
+                <p>FAQs</p>
+              </Link>
             </div>
-        </div>
+            <div className="Newsletter">
+              <h4>Others</h4>
+              <a href="">
+                <p>Terms & Conditions</p>
+              </a>
+              <a href="">
+                <p>Privacy Policy</p>
+              </a>
+            </div>
+          </div>
 
- 
+          <div className="Footerpt-3">
+            <h4>Newsletter</h4>
+            <div className="Footer-input-wrapper">
+              <input
+                type="text"
+                placeholder="Enter Your Email"
+                className="input-field"
+              />
+              <button type="button">Subscribe</button>
+            </div>
+            <h4>Phone: </h4>
+            <p> +1 (732) 451‑4720</p>
+            <h4>Address: </h4>
+            <p>4392 Riverside Ave Tampa, FL 33614</p>
+          </div>
         </div>
-  
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default Footer;
